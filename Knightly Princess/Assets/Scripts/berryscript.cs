d9f098f125d6d1c3398ad67f1c53e3f751bb2668
Lakria <maasheliite24@gmail.com>
2022-03-12 20:00:20 +0200

    Made a working quest
    talk to box on top left (E), go pick up berries in top right. Go back to top left and enjoy

--- Knightly Princess/Assets/Scripts/berryscript.cs
+++ Knightly Princess/Assets/Scripts/berryscript.cs
@@ -6,19 +6,21 @@ public class berryscript : MonoBehaviour
 {
     public Dialogue dialogue;
     public GameObject BerryPrize;
+    public GameObject Berries;
 
     private bool hasBerries;
     public void isBerries()
     {
         hasBerries = PlayerMovement.berries;
-        Debug.Log("Ha");
 
         if (hasBerries)
         {
-            Debug.Log("Ha");
             PlayerMovement.berries = false;
             BerryPrize.SetActive(true);
-            dialogue.sentences[0] = "Thank you for berries! Have this sword";
+            Berries.SetActive(false);
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            FindObjectOfType<PlayerMovement>().StopFighting();
+
         }
 
     }
