d941651acd93859fa4017ea2254096c08949ab67
Lakria <maasheliite24@gmail.com>
2022-03-12 20:47:50 +0200

    Put in the idle and walking animations for princess

--- Knightly Princess/Assets/Scripts/PlayerMovement.cs
+++ Knightly Princess/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Animator animator;
+
+
     public GameObject Slash;
     public Transform Slashpoint;
     private bool stopFighting;
@@ -40,13 +43,20 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+
         if (movement.x < 0)
         {
+            gameObject.transform.localRotation = Quaternion.Euler(0, 180, 0);
             direction.x = -1;
             direction.y = 0;
         }
         if (movement.x > 0)
         {
+            gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
             direction.x = 1;
             direction.y = 0;
         }
