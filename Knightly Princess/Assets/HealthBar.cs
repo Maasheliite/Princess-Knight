699ef0dc96282be2a2ee4a86c88b26efeb726515
Lakria <maasheliite24@gmail.com>
2022-06-13 21:23:40 +0300

    Added health bar

---
+++ Knightly Princess/Assets/HealthBar.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+
+    
+
+    void Update()
+    {
+        slider.value = ItemStatic.health;
+    }
+}
